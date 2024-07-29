aff7d5dbf22064c4af588c4f314b4816ae8796f5
markbuckle <markbuckle92@gmail.com>
2024-07-29 10:35:38 -0230

    submit query update

--- image/src/app_api_handler.py
+++ image/src/app_api_handler.py
@@ -7,9 +7,9 @@ from fastapi import FastAPI
 from mangum import Mangum
 from pydantic import BaseModel
 from query_model import QueryModel
-from rag_app.query_rag import query_rag
+from rag_app.query_rag import query_rag, QueryResponse
 
-WORKER_LAMBDA_NAME = os.environ.get("WORKER_LAMBDA_NAME", None)
+# WORKER_LAMBDA_NAME = os.environ.get("WORKER_LAMBDA_NAME", None)
 
 app = FastAPI()
 handler = Mangum(app)  # Entry point for AWS Lambda.
@@ -24,47 +24,47 @@ def index():
     return {"Hello": "World"}
 
 
-@app.get("/get_query")
-def get_query_endpoint(query_id: str) -> QueryModel:
-    query = QueryModel.get_item(query_id)
-    return query
+# @app.get("/get_query")
+# def get_query_endpoint(query_id: str) -> QueryModel:
+#     query = QueryModel.get_item(query_id)
+#     return query
 
 
 @app.post("/submit_query")
-def submit_query_endpoint(request: SubmitQueryRequest) -> QueryModel:
+def submit_query_endpoint(request: SubmitQueryRequest) -> QueryResponse:
     # Create the query item, and put it into the data-base.
-    new_query = QueryModel(query_text=request.query_text)
+    query_response = query_rag(query_text=request.query_text)
 
-    if WORKER_LAMBDA_NAME:
-        # Make an async call to the worker (the RAG/AI app).
-        new_query.put_item()
-        invoke_worker(new_query)
-    else:
-        # Make a synchronous call to the worker (the RAG/AI app).
-        query_response = query_rag(request.query_text)
-        new_query.answer_text = query_response.response_text
-        new_query.sources = query_response.sources
-        new_query.is_complete = True
-        new_query.put_item()
+    # if WORKER_LAMBDA_NAME:
+    #     # Make an async call to the worker (the RAG/AI app).
+    #     new_query.put_item()
+    #     invoke_worker(new_query)
+    # else:
+    #     # Make a synchronous call to the worker (the RAG/AI app).
+    #     query_response = query_rag(request.query_text)
+    #     new_query.answer_text = query_response.response_text
+    #     new_query.sources = query_response.sources
+    #     new_query.is_complete = True
+    #     new_query.put_item()
 
-    return new_query
+    return query_response
 
 
-def invoke_worker(query: QueryModel):
-    # Initialize the Lambda client
-    lambda_client = boto3.client("lambda")
+# def invoke_worker(query: QueryModel):
+#     # Initialize the Lambda client
+#     lambda_client = boto3.client("lambda")
 
-    # Get the QueryModel as a dictionary.
-    payload = query.model_dump()
+#     # Get the QueryModel as a dictionary.
+#     payload = query.model_dump()
 
-    # Invoke another Lambda function asynchronously
-    response = lambda_client.invoke(
-        FunctionName=WORKER_LAMBDA_NAME,
-        InvocationType="Event",
-        Payload=json.dumps(payload),
-    )
+#     # Invoke another Lambda function asynchronously
+#     response = lambda_client.invoke(
+#         FunctionName=WORKER_LAMBDA_NAME,
+#         InvocationType="Event",
+#         Payload=json.dumps(payload),
+#     )
 
-    print(f"✅ Worker Lambda invoked: {response}")
+#     print(f"✅ Worker Lambda invoked: {response}")
 
 
 if __name__ == "__main__":
